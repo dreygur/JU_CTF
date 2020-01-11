d211a94187c55a1522fbe7d6e9537a6e0cd9cf22
Rakibul Yeasin <rytotul@gmail.com>
2020-01-11 12:08:02 +0600

    Changed Structure a little bit

--- uni/names.go
+++ uni/names.go
@@ -1,19 +1,8 @@
-package main
+package uni
 
-import (
-	"encoding/json"
-	"fmt"
-	"math/rand"
-	"net/http"
-	"strings"
-)
+import "math/rand"
 
-// Form Id of the Google Form
-// formID := "1FAIpQLSegHaWfpaYRlhmcnKs_vQRyZCKoVLLKcw16DAA-l8i7CyJtwg"
-// Full URL of the Google Form
-// url := "https://docs.google.com/forms/d/e/" + formID + "/formResponse"
-
-func uname() (string, error) {
+func Uname() (string, error) {
 	// Returns Random Bangladeshi University name
 
 	// names := [10] string{
@@ -181,90 +170,4 @@ func uname() (string, error) {
 		"Exim Bank Agricultural University Bangladesh",
 		"Leading University")
 	return names[rand.Intn(len(names))], nil
-}
-
-func name(n int) string {
-	// This function returns some random names
-	chars := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_."
-	name := make([]byte, n) // Making Slices
-	for i := range name {
-		name[i] = chars[rand.Intn(len(chars))]
-	}
-
-	fmt.Println(string(name))
-	return string(name) // Stringify the output and return
-}
-
-func email(n int) string {
-	// Returns a email address
-
-	// Need to define more email addresses
-	domains := []string{"@gmail.com", "@yahoo.com", "@outlook.com", "@yandex.com", "@hotmail.com"}
-	fmt.Println(domains)
-	email := name(rand.Intn(n)) + domains[rand.Intn(len(domains))]
-	return email
-}
-
-func loweralpha() string {
-	// Generating alphanumeric charecters
-	p := make([]byte, 26) // Slice
-	for i := range p {
-		p[i] = 'a' + byte(i)
-	}
-	return string(p) // Return chars as string
-}
-
-func text() string {
-	// Generating random & dynamic Transaction ID's
-
-	// Currently shows error.
-	// Need to fix
-	tx := "TrxID " + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-	+rand.Int(9) + strings.ToUpper(loweralpha())
-
-	return txt[:16] // The first 16 chars are useful to us the rest are junk
-}
-
-func formData() (string, error) {
-	// This Function isn't implemented yet!
-	// Makes data organized and sendable as POST request t google form
-
-	req, err := json.MarshalIndent(map[string]string{
-		"entry.838304731":  "name",
-		"entry.2146648422": "2",
-		"entry.2129567656": "0",
-		"entry.1114548755": "0",
-		"entry.773910481":  "0",
-	}, "", "\t")
-
-	// _ = ioutil.WriteFile("test.json", req, 0644)
-
-	return string(req), err
-}
-
-func main() {
-	// Form Id of the Google Form
-	formID := "1FAIpQLSegHaWfpaYRlhmcnKs_vQRyZCKoVLLKcw16DAA-l8i7CyJtwg"
-	// Full URL of the Google Form
-	url := "https://docs.google.com/forms/d/e/" + formID + "/formResponse"
-	// Make request and get the Response
-	resp, err := http.Get(url)
-	if resp.StatusCode == 200 {
-		fmt.Printf("[+] %v -> %v\n", http.StatusText(resp.StatusCode), resp.StatusCode)
-	} else {
-		fmt.Println(err.Error())
-	}
-	data, err := formData()
-	fmt.Println(data, err)
-
-	fmt.Println(email(6))
-
-	fmt.Println(uname())
 }
